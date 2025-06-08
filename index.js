7bf0885c90b1c94a32afc82c2bcddf1930d8dbbf
Ellen Priscila <ellen.pri2004@gmail.com>
2025-06-08 12:56:29 -0300

    Adicionando o primeiro conteúdo ao index.html para testar se a aplicação vai funcionar no Vercel ou não, dessa vez

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const host = "0.0.0.0";
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, host, () => {
+    console.log(`Servidor em execução em http://${host}:${port}/`);
+});
