3e70e5b6bf865b68b40028aaee405a3849c86efa
Ellen Priscila <ellen.pri2004@gmail.com>
2025-06-08 18:01:25 -0300

    ai

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
